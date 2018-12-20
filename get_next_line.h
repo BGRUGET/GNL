5e6beb0addbc7b9a027e419feb0247d8a8eb42c8
BGRUGET <b.gruget@gmail.com>
2018-12-20 02:30:49 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: begruget <begruget@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/11/02 19:59:53 by begruget     #+#   ##    ##    #+#       */
+/*   Updated: 2018/12/20 02:13:51 by begruget    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+# include <fcntl.h>
+
+# define BUFF_SIZE 2
+
+int	get_next_line(const int fd, char **line);
+
+#endif
